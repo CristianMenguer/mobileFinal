9353c4b76d8f3253dae4e67ff57d3828db839b54
Cristian Menguer <cristianmenguer@gmail.com>
2020-10-03 10:24:12 +0100

    toast

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Text, View, Image } from 'react-native'
+import Toast from 'react-native-tiny-toast'
 
 import Styles from './style'
 
 const Home = () => {
+    Toast.show('test toast', {
+        position: Toast.position.BOTTOM,
+        duration: Toast.duration.SHORT,
+        containerStyle: {
+            borderRadius: 10,
+            width: '90%'
+        },
+        textStyle: {
+            color: '#e91e63'
+        },
+        animationDuration: 500
+    })
     return (
         <>
             <View style={Styles.container} >
